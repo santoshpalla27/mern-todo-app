8bbbcd83e5140fe77ee418a3dc37d37ab9c43312
santoshpalla27 <santoshpalla2002@gmail.com>
2024-12-21 03:15:43 +0530

    some changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // Ensure this line is present and correct
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
