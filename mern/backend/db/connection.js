210b3616a1d9d7ba28c6b592f957ff122fff5b93
santoshpalla27 <santoshpalla2002@gmail.com>
2024-12-25 20:52:29 +0530

    some changes

--- mern/backend/db/connection.js
+++ mern/backend/db/connection.js
@@ -1,19 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-// Fetch MongoDB connection details from environment variables
-const MONGO_USERNAME = process.env.MONGO_USERNAME;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_DB = process.env.MONGO_DB;
-
-if (!MONGO_USERNAME || !MONGO_PASSWORD || !MONGO_HOST || !MONGO_DB) {
-  throw new Error("MongoDB environment variables are not properly defined");
-}
-
-// Construct the MongoDB URI
-const URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${MONGO_DB}?authSource=admin`;
-
-// Initialize the MongoDB client
+const URI = "mongodb://mongodb:27017";
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,11 +16,9 @@ try {
   await client.db("admin").command({ ping: 1 });
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } catch (err) {
-  console.error("Error connecting to MongoDB:", err);
-  process.exit(1); // Exit with failure if connection fails
+  console.error(err);
 }
 
-// Use the specified database
-let db = client.db(MONGO_DB);
+let db = client.db("employees");
 
 export default db;
