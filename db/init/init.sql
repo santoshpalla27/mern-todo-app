ef589039fbedd79167f3570fd8dfacc70c84beca
santoshpalla27 <santoshpalla2002@gmail.com>
2024-12-25 15:06:55 +0530

    some changes

--- db/init/init.sql
+++
@@ -1,9 +0,0 @@
--- db/init/init.sql
-CREATE TABLE IF NOT EXISTS mytable (
-    id SERIAL PRIMARY KEY,
-    data TEXT NOT NULL
-);
-
-INSERT INTO mytable (data) VALUES ('Sample data 1');
-INSERT INTO mytable (data) VALUES ('Sample data 2');
-INSERT INTO mytable (data) VALUES ('Sample data 3');
