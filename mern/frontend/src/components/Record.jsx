d0990e710b74be72025425ab2ddcce85253f2b25
santoshpalla27 <santoshpalla2002@gmail.com>
2024-12-25 15:16:34 +0530

    some changes

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -17,7 +17,7 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://54.227.166.177:5050/record/${params.id.toString()}`
+        `${api-url}/record/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://54.227.166.177:5050/record", {
+        response = await fetch("${api-url}/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://54.227.166.177:5050/record/${params.id}`, {
+        response = await fetch(`${api-url}/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
